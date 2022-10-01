47cbaf251463b4d6aad653dceeaac2a27e29e083
Omkar <omkarthorve.ot@gmail.com>
2022-10-01 23:32:41 +0530

    updates 02/10/2022

--- quiz_game.py
+++ quiz_game.py
@@ -1,48 +1,47 @@
-import random
 import time
-def expression():
-    global exp
-    nums = [1,2,3,4,5,6,7,8,9]
-    opr = ["+","-","*","/","**"]
-    a = random.choice(nums)
-    b = random.choice(nums)
-    op= random.choice(opr)
-    print(a,op,b,end=" = ")
-    exp = eval("a"+op+"b")
+import random
 
+nums = ['1','2','3','4','5','6','7','8','9']
+opr = ["+", "-", "*"]
 
+print("Enter Total numbers of players")
+total_players = int(input())
+time_list = []
 
-
-
-print("*Welcome to Quiz Game*")
-print("Game Rules: ")
-print("1 - Each player will get 5 questions to solve")
-print("2 - If the answer is wrong, Re-Enter answer till it gets correct")
-print("3 - The player which will solve the quiz in less time will win the game")
-print("----------------------------------------------------------------------------------------------------\n")
-print("Player 1 Turn")
-expression()
-i=1
-while i<6:
-
-    pl_1 = input()
-    if str(pl_1)== str(exp):
-        if i==5:
-            break
-        else:
-            expression()
-    else:
+round = 1
+while round<=total_players:
+    print("Player", round, ", Ready...?")
+    print("Press ENTER to start your turn")
+    input()
+    st_time = time.perf_counter()
+    # questions starts from here
+    question=1
+    while question<=3:
+        a = random.choice(nums)
+        b = random.choice(nums)
+        op = random.choice(opr)
         while True:
-            if str(pl_1) != str(exp):
-                print("Incorrect answer, Please enter correct answer")
-
-                continue
-            else:
+            q = a + op + b
+            oans = eval(q)
+            print("Question No.",question)
+            print(q)
+            uans = int(input())
+            if uans == oans:
+                question = question + 1
+                print("Correct Answer...!")
                 break
+            else:
+                print("Wrong Answer, Try Again..!")
+                continue
+    else:
+        end_time = time.perf_counter()
+        total_time = end_time - st_time
+        time_list.append(total_time)
+        round = round + 1
+        print("Total Time taken :", total_time)
 
-
-
-
-    i=i+1
+min_time = min(time_list)
+winner_ind = time_list.index(min_time)
+print("Player-", (winner_ind+1), "wins...!")
 
 
